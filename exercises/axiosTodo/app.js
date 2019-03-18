856734a1e8337ca4ba73e3b8feedb6ba844ec97f
Darius Raazi <dariusraazi@icloud.com>
2019-03-18 16:20:47 -0600

    Updating my todo list

--- exercises/axiosTodo/app.js
+++ exercises/axiosTodo/app.js
@@ -4,3 +4,32 @@
 
 var toDo = axios.get("https://api.vschool.io/darius/todo")
 
+document.todo.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const title = document.todo.title.value;
+    const description = document.todo.description.value;
+   
+
+    const body = {
+        title,
+        description 
+    }
+
+    axios.post(url, body).then((response)=>{
+        console.log(response.data)
+    })
+})
+
+function createTodo(todo){
+    const container = document.createElement("div");
+    container.className = "container";
+
+    const title = document.createElement("h1");
+    title.className = "title";
+    title.textContent = todo.title;
+
+    const description = document.createElement("p");
+    description.className = "description";
+    description.textContent = todo.description;
+}
